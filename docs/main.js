298ee9ba91d8698564f0b5ac67442c4be7374ea3
Maria Diete <2010738297@stud.fh-kufstein.ac.at>
2021-05-20 21:38:33 +0200

    done

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,19 @@
-console.log("test");
+let pwaPrompt;
+const prompt = document.querySelector('#pwa-install-prompt');
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    pwaPrompt = e;
+    prompt.classList.add('show');
+})
+
+prompt.addEventListener('click', function(event) {
+    if (event.target.dataset.id === 'pwa-install-y' && pwaPrompt) {
+        pwaPrompt.prompt();
+        pwaPrompt.userChoice.then(() => {
+            prompt.classList.remove('show');
+            pwaPrompt = null;
+        });
+    } else {
+        prompt.classList.remove('show');
+    }
+});
