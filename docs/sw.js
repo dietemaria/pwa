43baaa2a116da2546e1b91cbcee4fe73cfe96dfb
Maria Diete <2010738297@stud.fh-kufstein.ac.at>
2021-05-20 21:33:41 +0200

    add serviceworker

---
+++ docs/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('mariapwa-v1')
+            .then(cache => cache.addAll([
+                'index.html',
+                'main.js',
+                'icon512.png',
+                'icon192.png',
+                'icon16.png',
+                'maskable-icon512.png',
+                'manifest.webmanifest'
+            ]))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open('mariaspwa-v1').then((cache) => {
+            return fetch(event.request)
+                .then((response) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                .catch(() => cache.match(event.request));
+        })
+    );
+});
